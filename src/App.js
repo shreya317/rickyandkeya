4a76e8f68a3ecbf706037e32345599a646657695
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2018-06-19 21:30:00 -0500

    apply gallery style affect

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='Main'>
         <NavBar onSelectedNavOption={selectedNavOption => {
           this.setState({selectedNavOption})
         }}/>

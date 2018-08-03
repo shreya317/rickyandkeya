878caea34694fb58b299c981bf19ea02288fc908
Shreya Patel <shreyapatel@ShreyaPatelsMBP.hsd1.il.comcast.net>
2018-08-03 14:16:34 -0500

    updates to accommodations page

--- src/Accommodations.js
+++ src/Accommodations.js
@@ -6,82 +6,108 @@ import './Accommodations.css'
 export default class Accommodations extends Component {
     render () {
         return (
-            <div className='Accommodations'>
-                <div className='Accommodations-main'>
-                    <div className='Comming-soon'>
-                        <h1 className="App-title">Coming soon...</h1> 
-                    </div>
-                </div>
-            </div>
-
             // <div className='Accommodations'>
-            //     <div className='Accommodations-title'>Accommodations</div>
-            //     <div>Join us for our wedding in paradise</div>
-            //     <div>Location: Hard Rock Hotel Riviera Maya Luxury All-Inclusive Resort</div>
-
-            //     <div><a href='https://gc.synxis.com/rez.aspx?Hotel=59660&Chain=13924&Dest=Riviera%20Maya&template=Groups&start=availresults&locale=en-US&arrive=4/9/2019&depart=4/15/2019&adult=2&child=0&group=190413PATE' target='_blank'>Click to Book Room</a></div>
-            //     <div><a href='https://www.shaadidestinations.com/flights-krupa-and-leslie' target='_blank'>Click to submit your flight details</a></div>
-                
-            //     <div>
-            //         <p> 
-            //             <b>Hard Rock Hotel Riviera Maya</b> is where luxury meets satisfaction on a stage of stunning tropical views, private white sand beaches with crystal clear water, and exclusive saltwater snorkeling areas. It’s all capped off with an inviting traditional design, legendary Hard Rock hospitality, and undeniable excess at every turn.
-            //             Live large at the Hard Rock Hotel in Riviera Maya and enjoy the very best of paradise for all. Or, pack your appetite for opulence and hit Heaven — an elevated and exclusive retreat for adult guests only featuring private adult beaches, superlative amenities, and dining at its finest.
-            //             Kick into high gear with activities from tennis and rock climbing to kayaks and tennis. Swim in style in the large sparkling pools with activities, snacks, and swim-up bar. Stay in shape at the fitness center or the Yoga Temple. Even rock stars need to relax too, so sit back and indulge in treatments from massages to facials to hydrotherapy at the lavish Rock Spa.
-            //             Dine in seven exceptional all-inclusive restaurants and let loose at three bars. Enjoy fresh and exotic ingredients buffet-style from around the world at The Market. Experience Mexican cuisine and regional specialties a la carte at Frida. Try everything from sushi to stir fry at Zen, and savor authentic Rodizio service and the flavors of Brazil at Ipanema. Relax with 24-hour room service. 
-            //         </p>
-
-            //         GROUP BOOKING DETAILS | MUST BOOK WITHIN GROUP BLOCK | BEST RATE GUARANTEED
-
-            //         <p>
-            //             Ricky and Keya have secured discounted group rates for guests of their wedding! <b>Suites must be booked through their group block on this site in order to secure the discounted rate, to secure included complimentary round-trip airport transportation to and from Hard Rock Riviera Maya, to be included in group promotions, and to be given priority access to rooming requests and be placed in the group block located near the bride and groom's group</b>. Guests attending wedding events MUST book through the group booking link. Reservations can be made until February 23rd, but please make your reservation by December 1st so we can plan for your attendance of events.
-            //         </p>
-            //         <p>
-            //             Please note rates are ALL-INCLUSIVE, per room, per night. Children 4-12 years old are $60.00 per child / per night. Children 13-17 years old are $ 120.00 per child / per night. <b>Child rates will be charged upon check in and will not display on your confirmation at the time of booking</b>. Children under 4 are free.
-            //         </p>
-            //         <p>
-            //             <b>Guests of Ricky and Keya booking through the group block also receive $1,000 resort credit per room when staying 3 nights, $1,500 resort credit per room at 4 nights, $3,000 at 5 or 6 nights, and $3,600 at 7 or 8 nights</b>. The more nights you stay in paradise, the more resort credit you receive to live life to the fullest with! Resort credit is limitless - after paying 20% service fees, the credit is applicable to all sorts of incredible experiences: massage and spa treatments, rounds of golf, exciting excursions on yachts or to Mayan ruins, and more!
-            //         </p>
-
-            //         <p>
-            //             While there is a 3 night minimum, most guests choose to add nights at the discounted rate before and after the wedding in order to enjoy the incredible resort with friends and family.
-            //         </p>
-
-            //         THE LEGENDARY HARD ROCK ALL-INCLUSIVE EXPERIENCE
-
-            //         <p>
-            //             Enjoy a service and style that know no limits. That’s how we roll with All Inclusive amenities including experiential dining at world-class restaurants, limitless rounds of championship golf, never-ending cocktails and in-room drinks from a personal liquor dispenser, legendary entertainment, and top-shelf everything across the board. It’s All Day. All Night. All Included, this is Hard Rock Riviera Maya baby.
-            //         </p>
-
-            //             <ul>
-            //                 <li> 5-Star accommodations </li>
-            //                 <li> Casual and à la carte dining </li>
-            //                 <li> Beverages (alcoholic and otherwise) </li>
-            //                 <li> Delicious snacks </li>
-            //                 <li> Top-shelf, in-room liquor dispenser </li>
-            //                 <li> 24/7 in-room service </li>
-            //                 <li> Limitless World-Class Golf </li>
-            //                 <li> Land sports </li>
-            //                 <li> Fitness center </li>
-            //                 <li> 'Born to Run' Jogging track </li>
-            //                 <li> Pool and beachside service </li>
-            //                 <li> Daily activities </li>
-            //                 <li> Nightly entertainment </li>
-            //                 <li> Live entertainment </li>
-            //                 <li> Free Wi-Fi in room and resort wide </li>
-            //                 <li> Parking Free </li>
-            //                 <li> Unlimited phone calls to the Continental US and Canada </li>
-            //                 <li> Gratuities included </li>
-            //             </ul>
-
-            //         CANCELLATION POLICY
-
-            //             <ul>
-            //                 <li> Cancellations made before February 22nd are fully refundable </li>
-            //                 <li> Cancellations made between February 23rd and March 26th will incur a 2 night charge </li>
-            //                 <li> Cancellations made after March 26th are non-refundable </li>
-            //             </ul>
+            //     <div className='Accommodations-main'>
+            //         <div className='Comming-soon'>
+            //             <h1 className="App-title">Coming soon...</h1> 
+            //         </div>
             //     </div>
             // </div>
+
+            <div className='Accommodations'>
+                <h1 className='Accommodations-title'>Accommodations</h1>
+                <h2 className='Accommodations-location'>Hard Rock Hotel Riviera Maya | Luxury All-Inclusive Resort</h2>
+
+                <div className='Accommodations-images'>
+                    <img src={require('./pictures/acc-resort.png')} width='700'/>
+                </div>
+
+                <div className='Accommodations-book'>
+                    <button><a href='https://www.shaadidestinations.com/reservation-ricky-and-keya' target='_blank'>CLICK HERE TO BOOK YOUR ROOM</a></button>
+                    <button><a href='https://www.shaadidestinations.com/flights-ricky-and-keya' target='_blank'>CLICK HERE TO SHARE YOUR FLIGHT INFORMATION</a></button>
+                </div>
+                
+                <div className='Accommodations-info'>
+                    <h3>Hard Rock Hotel Riviera Maya is where luxury meets satisfaction.</h3>
+                    <p>Ricky and Keya invite you to live large and enjoy the very best of paradise while celebrating their wedding on a stage of stunning tropical views with private white sand beaches and crystal-clear water.</p>
+                    <p>Come kick into high gear with activities from tennis and rock climbing to kayaking and golf. Swim in style in one of the many large sparkling pools with swim-up bars or take a little dive and snorkel in the lagoons. Indulge in treatments from massages to facials to hydrotherapy and satisfy your palate at any of the seven exceptional all-inclusive restaurants. Even spoil yourself with free 24-hour room service. </p>
+                    <p>Most important of all, get ready to party like a rock star with the bride and groom to be.</p>
+                    <hr/>
+                    <h3>GROUP BOOKING DETAILS</h3>
+                    <p>Ricky and Keya have secured discounted group rates for all of their guests through Shaadi Destinations.</p>
+                    <p>Reserve your room today for $200.00 / room by clicking the link above (final payments will be due at time of check-in). The above link will take you through Shaadi Destinations’ booking portal, where you will be able to select room type, amount and travel dates, to enter in number/names of guests and to securely pay your deposit.</p>
+                    <p>All rooms MUST be booked through the group block ON THIS SITE in order to secure the discounted rate and group promotions (including double resort credit), complimentary round-trip airport transportation, and access to all wedding events.</p>
+                    <p>Bookings made through other 3rd parties, private travel agents, or directly through the Hard Rock website will not be honored and an additional fee will be required to attend wedding events.</p>
+                    <p>All bookings must be made by February 1, 2019.</p>
+                    <hr/>
+                    <h3>ALL INCLUSIVE EXPERIENCE</h3>
+                    <p>Rates are ALL-INCLUSIVE, per room, per night and vary depending on which room type you select.</p>
+                    <ul>
+                        <li>Children under 4 are free</li>
+                        <li>Children 4-12 years old are $60.00 per child / per night. </li>
+                        <li>Children 13-17 years old are $ 120.00 per child / per night. </li>
+                    </ul>
+                    <p>All inclusive amenities include:</p>
+                    <ul>
+                        <li> 5-Star accommodations </li>
+                        <li> Casual and à la carte dining </li>
+                        <li> Beverages (alcoholic and otherwise) </li>
+                        <li> Delicious snacks </li>
+                        <li> Top-shelf, in-room liquor dispenser </li>
+                        <li> 24/7 in-room service </li>
+                        <li> Limitless World-Class Golf </li>
+                        <li> Land sports </li>
+                        <li> Fitness center </li>
+                        <li> 'Born to Run' Jogging track </li>
+                        <li> Pool and beachside service </li>
+                        <li> Daily activities </li>
+                        <li> Nightly entertainment </li>
+                        <li> Live entertainment </li>
+                        <li> Free Wi-Fi in room and resort wide </li>
+                        <li> Parking Free </li>
+                        <li> Unlimited phone calls to the Continental US and Canada </li>
+                        <li> Gratuities included </li>
+                    </ul>
+                    <hr/>
+                    <h3>CANCELLATION POLICY</h3>
+                    <p>Cancellations made before February 22nd, 2019 are fully refundable</p>
+                    <p>Cancellations made between February 23rd, 2019 and March 26th, 2019 will incur a 2-night charge</p>
+                    <p>Cancellations made after March 26th, 2019 are non-refundable</p>
+                    <hr/>
+                    <h3>FREQUENTLY ASKED QUESTIONS</h3>
+                    <ul>
+                        <li>
+                            <p><b>What are the visa and/or passport requirements for Mexico?</b></p>
+                            <p>Travel to Mexico requires valid passports, even for US Citizens. Please ensure that your passport expires after October 12, 2019 (6 months from the date of entry into Mexico).</p>
+                            <p>Mexico has no visa requirement for US Citizens, but you will be asked to fill out an FMM form (Mexico's visitor permit) on the plane or once you land. Once you get to immigration and customs at Cancún airport, the agents will give you the bottom half to retain until you leave the country. It is VERY important to keep this secure until you leave Mexico.</p>        
+                        </li>
+                        <li>
+                            <p><b>Is there transportation being provided between the airport and the resort?</b></p>
+                            <p>Yes - roundtrip airport transfers are being provided by Ricky and Keya for bookings made through the group reservation block. Please share your flight information with Shaadi Destinations as soon as you have flights reserved. A link to share your flight information can be found at the top of this page, or you can click <a href='https://www.shaadidestinations.com/flights-ricky-and-keya' target='_blank'>here.</a></p>
+                        </li>
+                        <li>
+                            <p><b>What time is check-in and check-out at Hard Rock Hotel Riviera Maya?</b></p>
+                            <p>Check-in time is at 3:00 pm and check-out time is at 12:00 pm. Reservations made through Shaadi Destinations will be given priority to early check-in times (however, this is not guaranteed)</p>
+                            <p>Guests are welcome to stay on property all day the day of check-in and check-out. Bags will be stored with butler staff when you don't have access to your room.</p>
+                        </li>
+                        <li>
+                            <p><b>Can I stay for just 2 nights?</b></p>
+                            <p>While most destination wedding guests choose to stay 3-5 nights when flying to Mexico, we understand there may be certain limiting factors. Unfortunately, group discounts are only valid on a minimum of 3-night stays. </p>
+                            <p>If you must stay 2 nights, please contact Shaadi Destinations by emailing hello@shaadidestinations.com with the subject line Ricky & Keya. Booking through Shaadi Destinations allows for certain group benefits to be conveyed and allows for the best rate possible.</p>
+                        </li>
+                        <li>
+                            <p><b>Are kids welcome?</b></p>
+                            <p>Ricky and Keya chose Hard Rock Hotel Riviera Maya because of the endless list of activities that will turn everyone into a child - so children are encouraged!</p>
+                            <p>Designed for kids between 4-12 years old, the Kids Club provides playful and safe activities so that the entire family finds their paradise in Riviera Maya. Activities include treasure hunts, origami creative sessions, movie nights, gardening, magic shows, and more.</p>
+                            <p>The Kids Club is loaded with all the amenities of an all-inclusive luxury resort, with fun, kid-oriented activities scheduled throughout the week, making for a vacation experience that parents will appreciate and children will never forget! Let them play and have fun in a secure environment while you enjoy too!</p>
+                        </li>
+                        <li>
+                            <p><b>What if I need help with my room booking?</b></p>
+                            <p>Ricky and Keya are working with a premiere Indian destination wedding planning company, Shaadi Destinations. You can contact Shaadi Destinations by e-mailing hello@shaadidestinations.com with the subject line Ricky & Keya if you need additional help or may have questions regarding bookings.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }

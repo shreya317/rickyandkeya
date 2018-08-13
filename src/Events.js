921959f6b4a4d0dbed0e9426dcdb1470716496e8
Shreya Patel <shreyapatel@Shreya-Patels-MacBook-Pro.local>
2018-08-12 21:00:58 -0600

    events

--- src/Events.js
+++ src/Events.js
@@ -24,6 +24,8 @@ export default class Events extends Component {
                     <p>9:30 AM onwards</p>
                     <p>Lunch will be served</p>
                     <p>Shree Swaminarayan Temple of Itasca</p>
+                    <p>21W 710 Irving Park Rd. Itasca, IL 60143</p>
+                    <button className='Events-button'><a href='https://www.google.com/maps/dir//Shree+Swaminarayan+Temple,+21W710+W+Irving+Park+Rd,+Itasca,+IL+60143/@41.9769597,-88.0518311,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fadd37ba45e8f:0xf61f103403f92fe!2m2!1d-88.0496424!2d41.9769597!3e0' target='_blank'>Click for directions</a></button>
                     <p>~</p>
                     <h3>Ricky's Manglik Prasango</h3>
                     <p>Saturday, April 6, 2019</p>
@@ -36,25 +38,22 @@ export default class Events extends Component {
                     <h2 className='Event-info-section'>RIVIERA MAYA, MEXICO</h2>
                     
                     <h3>Welcome Dinner & Sangeet</h3>
-                    <p>Help us kick off our wedding weekend with an evening full of food, dancing, and mehndi while enjoying the sunset over the beautiful Carribean Sea.</p>
                     <p>Friday, April 12, 2019</p>
                     <p>6:00 PM - 10:00 PM</p>
                     <p>Dinner will be served</p>
                     <p>Woodstock Terrace - Hard Rock Hotel Riviera Maya</p>
                     <p>~</p>
                     <h3>Wedding</h3>
-                    <p>Be witness as we finally tie the know and start our journey as a newly wedded couple.</p>
                     <p>Saturday, April 13, 2019</p>
-                    <p>10:00 AM Baraat at the Entrace of Hacienda Lobby</p>
-                    <p>11:00 AM Wedding ceremony at the Hacienda Beach</p>
-                    <p>12:30 PM Lunch at the Maracas Terrace</p>
+                    <p>10:00 AM - Baraat at the Entrace of Hacienda Lobby</p>
+                    <p>11:00 AM - Wedding ceremony at the Hacienda Beach</p>
+                    <p>12:30 PM - Lunch at the Maracas Terrace</p>
                     <p>Hacienda Beach - Hard Rock Hotel Riviera Maya</p>
                     <p>~</p>
                     <h3>Reception</h3>
-                    <p>Fill your bellies with some hour de vours and grab a few cocktails before you take your seats, because tonight is all about kicking back and letting loose while celebrating with a night full of fun, dancing, and unlimite partying.</p>
                     <p>Saturday, April 14, 2019</p>
-                    <p>6:00 PM Cocktails</p>
-                    <p>7:00 PM - 11:30 PM Reception</p>
+                    <p>6:00 PM - Cocktails</p>
+                    <p>7:00 PM - 11:30 PM - Reception</p>
                     <p>Dinner will be served</p>
                     <p>Convention Center Ballroom - Hard Rock Hotel Riviera Maya</p>
                 </div>

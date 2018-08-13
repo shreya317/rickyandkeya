76c2dadddbb731704e1b61d1a3136a6486ed8ed2
Shreya Patel <shreyapatel@ShreyaPatelsMBP.lan>
2018-08-13 14:09:53 -0600

    contact update

--- src/Contact.js
+++ src/Contact.js
@@ -11,7 +11,7 @@ export default class Contact extends Component {
                 <h1 className='Contact-title'>Contact Us</h1>
                 <div className='Contact-info'>
                     <p>We are working with a premiere Indian destination wedding planning company, Shaadi Destinations.</p> 
-                    <p>If you need additional help or may have questions regarding bookings, you may contact Shaadi Destinations</p>
+                    <p>If you need additional help or have questions regarding bookings, you may contact Shaadi Destinations</p>
                     <p>by e-mailing <a href="mailto:hello@shaadidestinations.com">hello@shaadidestinations.com</a> with the subject line Ricky & Keya.</p>
                     <p>If you have any other questions or concerns please email us at <a href="mailto:rickyandkeya@gmail.com">rickyandkeya@gmail.com</a>.</p>
                 </div>
